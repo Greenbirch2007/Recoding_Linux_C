09abc7a079d3d902f9a42944100ac8bb515afbbf
Greenbirch2007 <291109028@qq.com>
2019-09-18 19:03:01 +0800

    循环结构程序设计

--- TC/c11.c
+++ TC/c11.c
@@ -1,18 +1,9 @@
 #include<stdio.h>
+#include<string.h>
 
 void main()
 {
-	float x,amax,amin;
-	scanf("%f",&x);
-	amax=x;
-	amin=x;
-	while(x>=0.0)
-	{
-		if(x>amax)
-			amax=x;
-		if(amin>=x)
-			amin=x;
-		scanf("%f",&x);
-	}
-	printf("\n amax=%f \n amin=%f \n",amax,amin);
+	char s[] ={'1','2','3','\0'};
+	printf("%d\n",s);
 }
+
--- TC/c8.c
+++ TC/c8.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-#define MIX(a,b)((a)*(b)+(b))
 
-main()
+void main()
 {
-	int x=5,y=9;
-	printf("x,y\n");
-	printf("%d,%d\n",x,y);
-	printf("the min number is: %d\n",MIX(x,y));
+	char s[]={"123\n"};
 }
