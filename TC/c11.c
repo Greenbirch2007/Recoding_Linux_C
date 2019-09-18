3c4451456bf5ea69ed191c1b67dde7d23c7f8102
Greenbirch2007 <291109028@qq.com>
2019-09-18 12:58:02 +0800

    循环结构程序设计

--- TC/c11.c
+++ TC/c11.c
@@ -1,9 +1,18 @@
 #include<stdio.h>
 
 void main()
-
 {
-	int a,b;
-	scanf("%d%",&a,&b);
-	printf("a=%d,b=%d",a,b);
+	float x,amax,amin;
+	scanf("%f",&x);
+	amax=x;
+	amin=x;
+	while(x>=0.0)
+	{
+		if(x>amax)
+			amax=x;
+		if(amin>=x)
+			amin=x;
+		scanf("%f",&x);
+	}
+	printf("\n amax=%f \n amin=%f \n",amax,amin);
 }
